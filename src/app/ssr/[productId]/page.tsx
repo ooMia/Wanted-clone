e625553ce9fd62911270a832c01047a0301b9220
Hyeon-hak Kim <07ily@naver.com>
2023-09-11 16:05:52 +0900

    문제 1 & 2 이미지 링크

--- src/app/ssr/[productId]/page.tsx
+++ src/app/ssr/[productId]/page.tsx
@@ -1,4 +1,4 @@
-import Product from "../../../components/Product";
+    import Product from "../../../components/Product";
 
 async function getData(productId) {
 
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,18 +1,21 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Image from "next/image";
+import Link from "@mui/material/Link";
 
-export default function Product({item}) {
+const Product = ({item}) => {
     return (<Box>
-        <Image
-            alt={item.title}
-            src={item.thumbnail}
-            width={210}
-            height={118}
-            style={{
-                width: 210, height: 118, margin: 3
-            }}
-        />
+        <Link href={`/ssr/${item.id}`}>
+            <img
+                alt={item.title}
+                src={item.thumbnail}
+                style={{
+                    width: 210, height: 118, margin: 3,
+                }}
+            />
+        </Link>
+
         <Typography width={210} sx={{textOverflow: "ellipsis"}}>{item.title}</Typography>
     </Box>)
 }
+
+export default Product
